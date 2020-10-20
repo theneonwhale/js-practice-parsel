44f7d54ae8895193db15825171d3d6add79a9a69
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-20 17:07:27 +0300

    module 11

--- src/js/04-countries.js
+++
@@ -1,9 +0,0 @@
-import '../css/common.css';
-import '../css/countries.css';
-import countries from './countries.json';
-import itemsTemplate from '../templates/countries-items.hbs';
-
-const galleryRef = document.querySelector('.js-gallery');
-
-const markup = itemsTemplate(countries);
-galleryRef.insertAdjacentHTML('beforeend', markup);
