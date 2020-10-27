7965ad174f90b7e9cc51b4d8d3b618ae18cf0dfd
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-27 23:57:38 +0200

    m12

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -15,25 +15,59 @@
 
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-cards.hbs';
+import adventureTimeCardTpl from '../templates/adventure-time-cards.hbs';
 
 const refs = {
   cardContainer: document.querySelector('.js-card-container'),
+  cardATContainer: document.querySelector('.js-at-card-container'),
+  searchForm: document.querySelector('.js-search-form'),
 };
+
+refs.searchForm.addEventListener('submit', fetchPokemonById);
 
 // const r = fetch('https://pokeapi.co/api/v2/pokemon/9').then(data =>
 //   console.log(data),
 // );
 
-const r = fetch('https://pokeapi.co/api/v2/pokemon/667')
-  .then(response => {
-    return response.json();
-  })
-  .then(pokemon => {
-    console.log(pokemon);
-    const markup = pokemonCardTpl(pokemon);
-    console.log(markup);
-    refs.cardContainer.innerHTML = markup;
-  })
-  .catch(error => console.log(error));
+// const r = fetch('https://pokeapi.co/api/v2/pokemon/330')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(renderPokemonCard)
+//   .catch(error => console.log(error));
 
 // console.log(r);
+fetchPokemonById(45)
+  .then(renderPokemonCard)
+  .catch(error => console.log(error));
+
+fetchAdventureTimeById('finn')
+  .then(renderAdventureTimeCard)
+  .catch(error => console.log(error));
+
+function fetchPokemonById(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+    response => {
+      return response.json();
+    },
+  );
+}
+
+function fetchAdventureTimeById(adventureTimeId) {
+  return fetch(
+    `https://adventure-time-api.herokuapp.com/api/v1/characters/${adventureTimeId}`,
+  ).then(response => {
+    return response.json();
+  });
+}
+// .then(renderPokemonCard)
+// .catch(error => console.log(error));
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function renderAdventureTimeCard(adventure) {
+  refs.cardATContainer.innerHTML = adventureTimeCardTpl(adventure);
+}
