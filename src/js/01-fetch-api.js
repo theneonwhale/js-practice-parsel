b46b3c712c44505788514dd3d13510aab1215cd7
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-28 17:40:11 +0200

    m12

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -16,14 +16,24 @@
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-cards.hbs';
 import adventureTimeCardTpl from '../templates/adventure-time-cards.hbs';
+import API from './api-service'; //бэкенд
+import getRefs from './get-refs'; //рефы
 
-const refs = {
-  cardContainer: document.querySelector('.js-card-container'),
-  cardATContainer: document.querySelector('.js-at-card-container'),
-  searchForm: document.querySelector('.js-search-form'),
-};
+const refs = getRefs();
 
-refs.searchForm.addEventListener('submit', fetchPokemonById);
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  API.fetchPokemonById(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+  // console.log(form.elements);
+}
 
 // const r = fetch('https://pokeapi.co/api/v2/pokemon/9').then(data =>
 //   console.log(data),
@@ -37,29 +47,14 @@ refs.searchForm.addEventListener('submit', fetchPokemonById);
 //   .catch(error => console.log(error));
 
 // console.log(r);
-fetchPokemonById(45)
-  .then(renderPokemonCard)
-  .catch(error => console.log(error));
+// fetchPokemonById(45)
+//   .then(renderPokemonCard)
+//   .catch(error => console.log(error));
 
-fetchAdventureTimeById('finn')
+API.fetchAdventureTimeById('finn')
   .then(renderAdventureTimeCard)
   .catch(error => console.log(error));
 
-function fetchPokemonById(pokemonId) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
-    response => {
-      return response.json();
-    },
-  );
-}
-
-function fetchAdventureTimeById(adventureTimeId) {
-  return fetch(
-    `https://adventure-time-api.herokuapp.com/api/v1/characters/${adventureTimeId}`,
-  ).then(response => {
-    return response.json();
-  });
-}
 // .then(renderPokemonCard)
 // .catch(error => console.log(error));
 
@@ -71,3 +66,33 @@ function renderPokemonCard(pokemon) {
 function renderAdventureTimeCard(adventure) {
   refs.cardATContainer.innerHTML = adventureTimeCardTpl(adventure);
 }
+
+function onFetchError(error) {
+  alert('Error!!! Ups!');
+}
+
+//////////////////////Параметры запроса/////////////////////////
+
+fetch('https://pokeapi.co/api/v2/pokemon?offset=300&limit=100')
+  .then(r => r.json())
+  .then(console.log);
+
+fetch(
+  'https://pixabay.com/api?key=18468929-778868d20e0ea2a779d7d4913&q=cosmos&image_type=illustration',
+)
+  .then(r => r.json())
+  .then(console.log);
+
+const url = 'http://newsapi.org/v2/top-headlines?q=коронавирус';
+const options = {
+  headers: { 'X-Api-Key': '78a7a5d1d4074a089e9876225c6e9b0d' },
+};
+fetch(url, options)
+  .then(response => response.json())
+  .then(console.log);
+
+fetch(
+  'http://api.weatherstack.com/current?access_key=dc5b1cd5a04b8c218f2c43a23746cffe&query=Kiev',
+)
+  .then(response => response.json())
+  .then(console.log);
---
+++ src/js/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    cardContainer: document.querySelector('.js-card-container'),
+    cardATContainer: document.querySelector('.js-at-card-container'),
+    searchForm: document.querySelector('.js-search-form'),
+  };
+}
