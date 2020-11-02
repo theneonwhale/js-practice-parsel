566898a944a0333df896b684fbdaac5b0ddc5bef
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-11-02 23:59:05 +0200

    ь13

--- src/js/get-refs.js
+++
@@ -1,7 +0,0 @@
-export default function getRefs() {
-  return {
-    cardContainer: document.querySelector('.js-card-container'),
-    cardATContainer: document.querySelector('.js-at-card-container'),
-    searchForm: document.querySelector('.js-search-form'),
-  };
-}
